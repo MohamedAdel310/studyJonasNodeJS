071b704a42202c7e610e8367e18cb6f115e8c8a7
Mohamed Adel <ma7598101@gmail.com>
2023-02-16 14:17:38 +0200

    127. Managing Passwords npm i bcryptjs hash the password with cost of 12 delete password confirm field !!<bcrypt> is sync function so we wont it block the code so we use async function and await !!the value of encrypt to same password are defirent

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userShema = new mongoose.Schema({
   name: {
@@ -23,17 +24,27 @@ const userShema = new mongoose.Schema({
   },
   passwordConfirm: {
     type: String,
-    required: [true, 'Please confirm your password']
+    required: [true, 'Please confirm your password'],
+    validate: {
+      //this only works on CREATE and SAVE!!!
+      validator: function(val) {
+        return val === this.password;
+      },
+      message: `Passwords are not the same!`
+    }
   }
+});
+
+userShema.pre('save', async function(next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userShema);
 
 module.exports = User;
-
-// validate: {
-//   validator: function(val) {
-//     return val === this.password;
-//   },
-//   message: `wrong password`
-// }
