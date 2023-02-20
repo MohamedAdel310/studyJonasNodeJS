38feaca7c5aafa1144ac2bf8f36952f40c0c0435
Mohamed Adel <ma7598101@gmail.com>
2023-02-20 21:19:02 +0200

    136. Sending Emails with Nodemailer npm i nodemailer

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,7 +8,7 @@ router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 
 router.post('/forgot-password', authController.forgotPassword);
-// router.post('/login', authController.login);
+router.patch('/reset-password', authController.resetPassword);
 
 router
   .route('/')
